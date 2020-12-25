68536675d47e199abff4b2acaf5115afbbfba2bf
AmrAbdelmagid <amrabdelmagid900@gmail.com>
2020-12-25 15:12:45 +0200

    get the weather data

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 import '../services/location.dart';
+import '../services/networking.dart';
+
+const String kMyOpenWeatherApiKey = '964b68dc902da67f36d325abde0d4cf5';
+const String kOpenWeatherMapUrl = 'https://api.openweathermap.org/data/2.5/weather';
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -7,20 +11,20 @@ class LoadingScreen extends StatefulWidget {
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
-
   @override
   void initState() {
     super.initState();
-        getLocation();
+    getLocationData();
   }
 
-  void getLocation() async{
+  void getLocationData() async {
     Location location = Location();
     await location.getCurrentLocation();
-    print(location.getLatitude());
-    print(location.getLongitude());
+    NetworkHelper networkHelper = NetworkHelper(
+        url: '$kOpenWeatherMapUrl?lat=${location.getLatitude()}&lon=${location
+            .getLongitude()}&appid=$kMyOpenWeatherApiKey');
+    var weatherData = await networkHelper.getData();
   }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold();
